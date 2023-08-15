94b20f3e4053f6383ff43d616cecd2258de2826d
lazitech <104704916+lazitech@users.noreply.github.com>
2023-08-15 17:47:25 +0800

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,10 @@
+//输出
+console.log('Hello JavaScript')//控制台输出
+document.write('<h1>Hello JavaScript</h1>')//页面输出字符或者标签   (后端转前端)
+alert('Hello JavaScript')
+//输入
+prompt('姓名：')
+prompt('考号：')  
+//变量
+let name       //不可以重复声明
+var nname   //可以重复声明
